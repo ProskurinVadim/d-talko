a9149a35f8d04f08f374c10b4c9e3f1c652a3caa
ProskurinVadim <proskurin.vadim@gmail.com>
2025-02-06 03:40:49 +0200

    fixed typo error

--- app/components/views/Advantages/AdvantageButton/index.tsx
+++ app/components/views/Advantages/AdvantageButton/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 const AdvantageButton:FC<object> = () => {
     const router = useRouter();
     return(
-        <Button className="block  mx-auto mt-8 md:mt-10 lg:mt-[52px]" onClick={()=>  router.push('/about')} inverse={true}>Детальніше про лікаря</Button>
+        <Button className="block  mx-auto mt-8 md:mt-10 lg:mt-[52px]" onClick={()=>  router.push('/about')} tlwVar={{type: "inverse"}}>Детальніше про лікаря</Button>
     )
 }
 
